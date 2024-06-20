a7b2e5f16b25cfa4654eb82110bba84f1860f572
Vaibhav Deokar <vaibhav.deokar@edas.tech>
2024-06-20 16:36:16 +0530

    Add initial project files

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Griddy Movies
+
+web application to watch series and films for free, builded with the [TMDB API](https://developer.themoviedb.org/docs/getting-started)
+
+## Features
+
+- Subtitles in (almost) every language
+- Fullscreen mode
+- Fully Responsive
+- FREE
+
+## Screenshots
+
+![mainScreen](https://github.com/silentm4gician/griddy-movies/assets/112287756/fd8a7376-fe71-4a3a-9f93-e69209975f14)
+![image](https://github.com/silentm4gician/griddy-movies/assets/112287756/c74873fe-440a-4ccf-aa58-86fbee9cd508)
+
+## Roadmap
+
+- Add functions to create and administrate Users
+- Add User autentication
+- Add playlists (to watch, watching, plan to watch, etc)
+- Implement comments, likes and dislikes
+
+## Authors
+
+- [@silentM4gician](https://github.com/silentm4gician)
+- [@Tropicaal_](https://github.com/Luks-code)
+
+
---
+++ src/api/requests/requests.js
@@ -0,0 +1,148 @@
+import axios from "axios";
+
+export const getTrending = async () => {
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/trending/all/day",
+    params: { language: "en-US" },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.request(options);
+
+  return res.data;
+};
+
+export const getTrendingMovies = async () => {
+  // console.log('API Key:', process.env.NEXT_PUBLIC_ACCESS_TOKEN);  // Log the API key to ensure it's being read correctly
+
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/movie/popular",
+    params: { language: "en-US", page: "1" },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.request(options);
+
+  return res.data;
+};
+
+export const getTopRatedTvShows = async () => {
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/tv/top_rated",
+    params: { language: "en-US", page: "1" },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.request(options);
+
+  return res.data;
+};
+
+export const getTopRatedMovies = async () => {
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/movie/top_rated",
+    params: { language: "en-US", page: "1" },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.request(options);
+
+  return res.data;
+};
+
+export const getMovieInfo = async (movieId) => {
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/movie/${movieId}`,
+    params: { language: "en-US" },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.request(options);
+
+  return res.data;
+};
+
+export const getShowInfo = async (showId) => {
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/tv/${showId}`,
+    params: { language: "en-US" },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.request(options);
+
+  return res.data;
+};
+
+export const getSeasonInfo = async (showId, showSeason) => {
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/tv/${showId}/season/${showSeason}`,
+    params: { language: "en-US" },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.request(options);
+
+  return res.data;
+};
+
+export const getMovieResults = async (toSearch) => {
+  const options = {
+    method: "GET",
+    url: 'https://api.themoviedb.org/3/search/movie',
+    params: { query: toSearch, include_adult: 'false', language: 'en-US', page: '1' },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.request(options);
+
+  return res.data;
+};
+
+export const getTvResults = async (toSearch) => {
+  const options = {
+    method: "GET",
+    url: 'https://api.themoviedb.org/3/search/tv',
+    params: { query: toSearch, include_adult: 'false', language: 'en-US', page: '1' },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.request(options);
+
+  return res.data;
+};
+
